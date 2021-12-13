f5c61e17209556f147ce456cd4e3aa9f18dab66c
Irving Morales <irvingfisica@gmail.com>
2021-12-13 13:06:50 -0600

    arboles kdtree

--- src/geounidades.rs
+++ src/geounidades.rs
@@ -2,18 +2,24 @@ use std::error::Error;
 use std::collections::HashMap;
 use shapefile::dbase::{Record,FieldValue};
 use geo::algorithm::centroid::Centroid;
+use kdtree::KdTree;
 
 use crate::geodata::Datos;
+
+pub trait GeoTool {
+    fn agregar_rama(&self, arbol: &mut KdTree<f64, String, [f64;2]>) -> Result<(), Box<dyn Error>>;
+}
 
 #[derive(Clone,Debug)]
 pub struct GeoPoligono<T: Datos> {
     pub poligono: geo::Polygon<f64>,
     pub datos: Option<T>,
     pub centroide: Option<geo::Point<f64>>,
+    pub cve: Option<String>,
 }
 
 impl<T: Datos> GeoPoligono<T> {
-    pub fn new(geopol: &geo::Polygon<f64>, datos: Option<T>) -> Self {
+    pub fn new(geopol: &geo::Polygon<f64>, datos: Option<T>, cve: Option<String>) -> Self {
 
         let centroide = geopol.centroid();
 
@@ -21,7 +27,23 @@ impl<T: Datos> GeoPoligono<T> {
             poligono: geopol.clone(),
             datos: datos,
             centroide: centroide,
+            cve: cve,
         }
+    }
+}
+
+impl<T: Datos> GeoTool for GeoPoligono<T> {
+    fn agregar_rama(&self, arbol: &mut KdTree<f64, String, [f64;2]>) -> Result<(), Box<dyn Error>> {
+
+        match (self.centroide,self.cve.as_ref()) {
+            (Some(punto),Some(cve)) => {
+                let coord = [punto.x(),punto.y()];
+                arbol.add(coord,cve.clone())?;
+            },
+            _ => {}
+        };
+
+        Ok(())
     }
 }
 
@@ -36,14 +58,17 @@ pub fn read_polygons<T: Datos>(ruta: &str, mapa: &HashMap<String,T>, cveid: &str
         let geo_polygon: geo::MultiPolygon<f64> = polygon.into();
         for geopol in geo_polygon {
 
+            let mut cve = None; 
+
             let datos = match record.get(cveid) {
                 Some(FieldValue::Character(Some(clave))) => {
+                    cve = Some(clave.clone());
                     mapa.get(clave).cloned()
                 },
                 _ => None,
             };
 
-            let poltemp = GeoPoligono::new(&geopol, datos);
+            let poltemp = GeoPoligono::new(&geopol, datos, cve);
             salida.push(poltemp);
         }
     }
@@ -54,17 +79,34 @@ pub fn read_polygons<T: Datos>(ruta: &str, mapa: &HashMap<String,T>, cveid: &str
 
 #[derive(Clone,Debug)]
 pub struct GeoPunto<T: Datos> {
-    pub punto: geo::Point<f64>,
+    pub punto: Option<geo::Point<f64>>,
     pub datos: Option<T>,
+    pub cve: Option<String>,
 }
 
 impl<T: Datos> GeoPunto<T> {
-    pub fn new(geopoint: &geo::Point<f64>, datos: Option<T>) -> Self {
+    pub fn new(geopoint: Option<geo::Point<f64>>, datos: Option<T>, cve: Option<String>) -> Self {
 
         GeoPunto {
             punto: geopoint.clone(),
             datos: datos,
+            cve: cve,
         }
+    }
+}
+
+impl<T: Datos> GeoTool for GeoPunto<T> {
+    fn agregar_rama(&self, arbol: &mut KdTree<f64, String, [f64;2]>) -> Result<(), Box<dyn Error>> {
+
+        match (self.punto,self.cve.as_ref()) {
+            (Some(punto),Some(cve)) => {
+                let coord = [punto.x(),punto.y()];
+                arbol.add(coord,cve.clone())?;
+            },
+            _ => {}
+        };
+
+        Ok(())
     }
 }
 
@@ -73,15 +115,21 @@ pub fn points_from_vec<T: Datos>(vector: Vec<T>) -> Result<Vec<GeoPunto<T>>, Box
 
     let mut salida: Vec<GeoPunto<T>> = Vec::new();
 
+    let mut contador: usize = 0;
+
     for evento in vector {
         let coords = match evento.get_coordinates() {
-            Some((lon,lat)) => (lon,lat),
+            Some((lon,lat)) => {
+                contador = contador + 1;
+                (lon,lat)
+            },
             _ => break
         };
 
-        let punto = geo::Point::new(coords.0, coords.1);
+        let punto = Some(geo::Point::new(coords.0, coords.1));
+        let cve = contador.to_string();
 
-        let pointemp = GeoPunto::new(&punto, Some(evento));
+        let pointemp = GeoPunto::new(punto, Some(evento), Some(cve));
         salida.push(pointemp);
     };
 
