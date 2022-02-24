e190fb9fb41f30eea49606ea07edf8c8ede0af19
Irving Morales <irvingfisica@gmail.com>
2022-02-24 15:44:41 -0600

    no main

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,25 @@
-use std::collections::HashMap;
-use manzanero::geodata;
-use manzanero::geounidades;
-use geodata::{ManzanaCenso2020,CarpetaInvestigacionCDMX};
-use geounidades::{GeoPoligono,GeoPunto};
-#[cfg(feature = "proj")]
-use geounidades::Projector;
+// use std::collections::HashMap;
+// use manzanero::geodata;
+// use manzanero::geounidades;
+// use geodata::{ManzanaCenso2020,CarpetaInvestigacionCDMX};
+// use geounidades::{GeoPoligono,GeoPunto};
+// #[cfg(feature = "proj")]
+// use geounidades::Projector;
 
 fn main() {
 
-    let mut carpetas: Vec<CarpetaInvestigacionCDMX> = Vec::new();
+    // let mut carpetas: Vec<CarpetaInvestigacionCDMX> = Vec::new();
 
-    geodata::read_carpetascdmx_csv("./datos/carpetas_completa_octubre_2021.csv", &mut carpetas).unwrap();
-    let _eventos: HashMap<String, GeoPunto<CarpetaInvestigacionCDMX>> = geounidades::points_from_vec(carpetas).unwrap();
+    // geodata::read_carpetascdmx_csv("./datos/carpetas_completa_octubre_2021.csv", &mut carpetas).unwrap();
+    // let _eventos: HashMap<String, GeoPunto<CarpetaInvestigacionCDMX>> = geounidades::points_from_vec(carpetas).unwrap();
 
-    let mut mapa: HashMap<String,ManzanaCenso2020> = HashMap::new();
+    // let mut mapa: HashMap<String,ManzanaCenso2020> = HashMap::new();
 
-    geodata::read_mzacenso2020_csv("./datos/RESAGEBURB_09CSV20.csv", &mut mapa).unwrap();
-    let manzanas: HashMap<String, GeoPoligono<ManzanaCenso2020>> = geounidades::read_polygons("./datos/09m.shp", &mapa, "CVEGEO").unwrap();
+    // geodata::read_mzacenso2020_csv("./datos/RESAGEBURB_09CSV20.csv", &mut mapa).unwrap();
+    // let manzanas: HashMap<String, GeoPoligono<ManzanaCenso2020>> = geounidades::read_polygons("./datos/09m.shp", &mapa, "CVEGEO").unwrap();
 
-    let mantest = manzanas.get("0900200010010001").unwrap().clone();
-    println!("Projectadas: {:?}\n",mantest.poligono);
+    // let mantest = manzanas.get("0900200010010001").unwrap().clone();
+    // println!("Projectadas: {:?}\n",mantest.poligono);
 
     // let prjstr = "+proj=lcc +lat_0=12 +lon_0=-102 +lat_1=17.5 +lat_2=29.5 +x_0=2500000 +y_0=0 +ellps=GRS80 +units=m +no_defs";
 
